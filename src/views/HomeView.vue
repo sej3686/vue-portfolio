97a0cf6328dc634d1e41a2b71058dfc18337f887
FisterMedister <stefanjespersen8@gmail.com>
2024-05-12 11:02:47 +0200

    updated keywords and misc

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -78,9 +78,9 @@
 
         <h4>
           I'm a Junior Web Developer <br />
-          with various experiences in
+          <!-- with various experiences in
           <br />
-          both web-design and multi-media
+          both web-design and multi-media -->
         </h4>
 
         <h5>I have experiance in:</h5>
@@ -102,6 +102,7 @@
           <img src="../assets/img/icons/sql.png" alt="sql logo" />
           <img src="../assets/img/icons/c-sharp.png" alt="c sharp logo" />
         </div>
+        <a class="call-to-action" href="#keyword"><button>Check out my website!</button></a>
       </section>
     </header>
 
@@ -110,7 +111,8 @@
 
       <main class="keyword-grid-wrapper reveal">
         <div class="keyword-grid-item">
-          <img class="img-1" src="../assets/img/code.png" alt="code icon" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>
+          <!-- <img class="img-1" src="../assets/img/code.png" alt="code icon" /> -->
           <h3>Coder</h3>
           <p>
             What I do when I'm not busy doing other things, is coding for fun.
@@ -118,7 +120,8 @@
           </p>
         </div>
         <div class="keyword-grid-item">
-          <img src="../assets/img/gallery.png" alt="gallery icon" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/></svg>
+          <!-- <img src="../assets/img/gallery.png" alt="gallery icon" /> -->
           <h3>Designer</h3>
           <p>
             Because of my earlier degree. I have experience in various Adobe
@@ -126,7 +129,8 @@
           </p>
         </div>
         <div class="keyword-grid-item">
-          <img src="../assets/img/handshake.png" alt="handshake icon" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M272.2 64.6l-51.1 51.1c-15.3 4.2-29.5 11.9-41.5 22.5L153 161.9C142.8 171 129.5 176 115.8 176H96V304c20.4 .6 39.8 8.9 54.3 23.4l35.6 35.6 7 7 0 0L219.9 397c6.2 6.2 16.4 6.2 22.6 0c1.7-1.7 3-3.7 3.7-5.8c2.8-7.7 9.3-13.5 17.3-15.3s16.4 .6 22.2 6.5L296.5 393c11.6 11.6 30.4 11.6 41.9 0c5.4-5.4 8.3-12.3 8.6-19.4c.4-8.8 5.6-16.6 13.6-20.4s17.3-3 24.4 2.1c9.4 6.7 22.5 5.8 30.9-2.6c9.4-9.4 9.4-24.6 0-33.9L340.1 243l-35.8 33c-27.3 25.2-69.2 25.6-97 .9c-31.7-28.2-32.4-77.4-1.6-106.5l70.1-66.2C303.2 78.4 339.4 64 377.1 64c36.1 0 71 13.3 97.9 37.2L505.1 128H544h40 40c8.8 0 16 7.2 16 16V352c0 17.7-14.3 32-32 32H576c-11.8 0-22.2-6.4-27.7-16H463.4c-3.4 6.7-7.9 13.1-13.5 18.7c-17.1 17.1-40.8 23.8-63 20.1c-3.6 7.3-8.5 14.1-14.6 20.2c-27.3 27.3-70 30-100.4 8.1c-25.1 20.8-62.5 19.5-86-4.1L159 404l-7-7-35.6-35.6c-5.5-5.5-12.7-8.7-20.4-9.3C96 369.7 81.6 384 64 384H32c-17.7 0-32-14.3-32-32V144c0-8.8 7.2-16 16-16H56 96h19.8c2 0 3.9-.7 5.3-2l26.5-23.6C175.5 77.7 211.4 64 248.7 64H259c4.4 0 8.9 .2 13.2 .6zM544 320V176H496c-5.9 0-11.6-2.2-15.9-6.1l-36.9-32.8c-18.2-16.2-41.7-25.1-66.1-25.1c-25.4 0-49.8 9.7-68.3 27.1l-70.1 66.2c-10.3 9.8-10.1 26.3 .5 35.7c9.3 8.3 23.4 8.1 32.5-.3l71.9-66.4c9.7-9 24.9-8.4 33.9 1.4s8.4 24.9-1.4 33.9l-.8 .8 74.4 74.4c10 10 16.5 22.3 19.4 35.1H544zM64 336a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm528 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32z"/></svg>
+          <!-- <img src="../assets/img/handshake.png" alt="handshake icon" /> -->
           <h3>Friendly</h3>
           <p>
             All of my former work places have appreciated how friendly and
