a500c2df140aa70d8c992b8ef97dd3e6d8f044fe
FisterMedister <stefanjespersen8@gmail.com>
2025-01-05 15:35:51 +0100

    found the error where I accidentally made all anchors display none adammit :(

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,44 +1,51 @@
 <template>
   <div class="projects">
-    <h1>Work In Progress</h1>
+    <h1>Work in Progress</h1>
     <RouterLink to="/"><button>Go Back</button></RouterLink>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .projects {
-    align-items: center;
-    justify-items: center;
-    display: flex;
-    height: 100vh;
-    flex-direction: column;
-    justify-content: center;
-    width: 100%;
+.projects {
+  align-items: center;
+  justify-items: center;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
 
-    h1 {
-      color: #39b54a;
-      font-size: 5rem;
-    }
-
-    button {
-      background-color: transparent;
-      border: 3px solid #39b54a;
-      border-radius: 30px;
-      color: #ffffff;
-      font-weight: 700;
-      font-size: 1.1rem;
-      height: 40px;
-      margin-top: 2em;
-      min-width: 220px;
-      transition: 0.2s;
-    }
-
-    button:hover {
-      background-color: #ffffff;
-      color: #000000;
-      cursor: pointer;
-    }
+  h1 {
+    color: #39b54a;
+    font-size: 5rem;
   }
+
+  button {
+    background-color: transparent;
+    border: 3px solid #39b54a;
+    border-radius: 30px;
+    color: #ffffff;
+    font-weight: 700;
+    font-size: 1.1rem;
+    height: 40px;
+    margin-top: 2em;
+    min-width: 220px;
+    transition: 0.2s;
+  }
+
+  button:hover {
+    background-color: #ffffff;
+    color: #000000;
+    cursor: pointer;
+  }
+}
+
+@media (max-width: 640px) {
+  .projects {
+    h1 {
+    font-size: 2.5rem;
+  }
+  }
+
 }
 </style>
