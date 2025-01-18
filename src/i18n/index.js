1e17a9dd54ccf525ce5489056813c618067d400b
FisterMedister <stefanjespersen8@gmail.com>
2025-01-18 16:55:35 +0100

    updated i18n so it now detects users browser language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,15 +1,40 @@
-import { createI18n } from "vue-i18n";
-import pluralRules from "./rules/pluralization"
-import da from "./locales/da.json"
+import { createI18n } from "vue-i18n"; 
+import pluralRules from "./rules/pluralization";
 
-const messages = {}
+function guessDefaultLocale() {
+  const supportedLocales = import.meta.env.VITE_SUPPORTED_LOCALES.split(",");
+  const defaultLocale = import.meta.env.VITE_DEFAULT_LOCALE;
 
+  const persistedLocale = localStorage.getItem("user-locale");
+  if (supportedLocales.includes(persistedLocale)) {
+    return persistedLocale;
+  }
+
+  const browserLocale = navigator.language || navigator.userLanguage;
+  const localeNoRegion = browserLocale.split("-")[0];
+  if (supportedLocales.includes(browserLocale)) {
+    return browserLocale;
+  }
+  if (supportedLocales.includes(localeNoRegion)) {
+    return localeNoRegion;
+  }
+
+  return defaultLocale;
+}
+
+const messages = {};
+
+const defaultLocale = guessDefaultLocale();
+(async () => {
+  const defaultMessages = await import(`./locales/${defaultLocale}.json`);
+  messages[defaultLocale] = defaultMessages.default;
+})();
 
 export default createI18n({
-  locale: import.meta.env.VITE_DEFAULT_LOCALE,
+  locale: defaultLocale,
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
   legacy: false,
   globalInjection: true,
-  messages: { da },
-  pluralRules
-})
+  messages,
+  pluralRules,
+});
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import i18n from './i18n'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import i18n from './i18n';
 
-import './assets/style.scss'
-import './assets/sass/reset.scss'
+import './assets/style.scss';
+import './assets/sass/reset.scss';
 
-createApp(App).
-    use(router).
-    use(i18n).
-    mount('#app')
+(async () => {
+  const defaultLocale = i18n.global.locale.value;
+
+  const messages = await import(`./i18n/locales/${defaultLocale}.json`);
+
+  i18n.global.setLocaleMessage(defaultLocale, messages.default);
+
+  createApp(App)
+    .use(router)
+    .use(i18n)
+    .mount('#app');
+})();
